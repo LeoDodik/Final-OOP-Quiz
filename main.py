c28fbe6054c0c52e242307e52c43d0f15073649c
LeoDodik <119758958+LeoDodik@users.noreply.github.com>
2024-03-02 11:36:11 +0100

    Final Files

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 from question_model import Question
 from data import question_data
-
+from quiz_brain import QuizBrain
 question_bank = []
 
 
@@ -10,3 +10,16 @@ for question in question_data:
     just_answer = question['answer']
     new_question = Question(just_question,just_answer)
     question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
+
+
+
+while quiz.still_has_questions():
+    quiz.next_question()
+if quiz.still_has_questions() == False:
+    print("You've completed the quiz")
+    print(f"Your final score was {quiz.score}/{quiz.question_number}")
+
+
